743b9bffc2ebf2db8fa759830deed556505a3579
Josue <nmartel@fi.uba.ar>
2025-06-22 20:20:35 -0300

    fix(topology): fix switches on startup

--- topology.py
+++ topology.py
@@ -3,26 +3,30 @@ from mininet.topo import Topo
 HOSTS_COUNT = 4
 
 class Topology(Topo):
-    def build(self, switches_count):
-        if switches_count < 2:
-            print("[ERROR] We need at least 2 switches.")
-            exit(-1)
+    def build(self, *args, **params):
+            switches_count = params.get('switches_count', 2)
+            if switches_count < 2:
+                print("[ERROR] We need at least 2 switches.")
+                exit(-1)
+    
+            # Crear hosts
+            hosts = []
+            for host in range(HOSTS_COUNT):
+                hosts.append(self.addHost("host%s" % host))
+    
+            # Crear switches
+            switches = []
+            for i in range(switches_count):
+                switches.append(self.addSwitch("s%s" % (i + 1)))
+    
+            # Conectar switches en cadena
+            for i in range(1, switches_count):
+                self.addLink(switches[i-1], switches[i])
+    
+            # Conectar hosts
+            self.addLink(hosts[0], switches[0])
+            self.addLink(hosts[1], switches[0])
+            self.addLink(hosts[2], switches[-1])
+            self.addLink(hosts[3], switches[-1])
 
-        hosts=[]
-        for host in range(HOSTS_COUNT):
-            hosts.append(self.addHost(f"host_id{host}" ))
-
-        switches=[]
-
-        for i in range(switches_count):
-            switches.append(self.addSwitch(f"switch_id{i}"))
-
-        for i in range(1, switches_count):
-            self.addLink(switches[i-1], switches[i])
-
-        self.addLink(hosts[0], switches[0])
-        self.addLink(hosts[1], switches[0])
-        self.addLink(hosts[2], switches[-1])
-        self.addLink(hosts[3], switches[-1])
-
-topos={"topo":Topology}
+topos={"topologia":Topology}
